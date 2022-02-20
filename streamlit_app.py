ce593a4d139d04678a757c5c1cba7e24daa9affa
Healy Dwyer <healydwyer@gmail.com>
2022-02-19 21:47:41 -0500

    adding urls for AV data sets

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ st.title("Let's analyze some Penguin Data 🐧📊.")
 @st.cache  # add caching so we load the data only once
 def load_data():
     # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
+    # TODO: Change this to load https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-h-j/master/2017_AV_data.csv 
+    # and https://raw.githubusercontent.com/CMU-IDS-2022/assignment-2-h-j/master/2019_AV_data.csv
     penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
     return pd.read_csv(penguins_url)
 
